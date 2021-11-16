25fc64d387472926fce8af8b70cb658e83ecaa20
shital bramhe <shitalbramhe@gmail.com>
2021-11-16 22:17:42 +0530

    [shital] : nlog

---
+++ NLogproject/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace NLogproject
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            AddNumbers add = new AddNumbers();
+            add.Sum(12, 23);
+        }
+    }
+}
